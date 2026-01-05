e951e5b5265551c04be0c92dc1126b77ed5f4c0e
teresakissel <teresakissel02@gmail.com>
2026-01-04 19:40:12 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,5 +3,14 @@ import os
 
 FILE_NAME = "tasks.json"
 
-def create_task():
+def create_task(tasks):
+  task_id = len(tasks) + 1
+  title = input("Enter task title")
+  desc = input("Enter task description")
+  priority = input("Enter task priority (Low/Medium/High/Urgent)")
+  due_date = input("Enter task due date")
+
+task = {
+  "id" = task_id
+      
   
